615e999022d3826ffc27f9fa5ba39b5987d756a4
kalyanramudu <kalyanramudu04@gmail.com>
2023-01-09 07:43:31 +0000

    changes

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -19,25 +19,12 @@ class DatabaseConfig {
         }
     }
 
+    public final static String DRIVER_NAME = prop.getProperty("db.driver");
+    public final static String DB_HOST = System.getProperty("RDS_HOSTNAME");
+    public final static String DB_PORT = System.getProperty("RDS_PORT");
+    public final static String DB_NAME = System.getProperty("RDS_DB_NAME");
+    public final static String DB_USER_NAME = System.getProperty("RDS_USERNAME");
+    public final static String DB_PASSWORD = System.getProperty("RDS_PASSWORD");
+    public final static String CONNECTION_STRING = DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
 
-private static Connection getRemoteConnection() {
-    if (System.getProperty("RDS_HOSTNAME") != null) {
-      try {
-      Class.forName("org.mysql.Driver");
-      String dbName = System.getProperty("RDS_DB_NAME");
-      String userName = System.getProperty("RDS_USERNAME");
-      String password = System.getProperty("RDS_PASSWORD");
-      String hostname = System.getProperty("RDS_HOSTNAME");
-      String port = System.getProperty("RDS_PORT");
-      String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-      logger.trace("Getting remote connection with connection string from environment variables.");
-      Connection con = DriverManager.getConnection(jdbcUrl);
-      logger.info("Remote connection successful.");
-      return con;
-    }
-    catch (ClassNotFoundException e) { logger.warn(e.toString());}
-    catch (SQLException e) { logger.warn(e.toString());}
-    }
-    return null;
-  }
 }
